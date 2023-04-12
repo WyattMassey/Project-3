1e0f565f0cd398c2bdec3bedf4ca17c05adc800e
WyattMassey <130611144+WyattMassey@users.noreply.github.com>
2023-04-12 17:35:09 -0500

    Create player.h

---
+++ player.h
@@ -0,0 +1,60 @@
+class Player{
+
+  private:
+  
+  string playerName;
+  int playerFullness;
+  int playerGold;
+  int playerCeramicPot;
+  int playerFryingPan;
+  int playerCauldron;
+  int playerClub;
+  int playerSpear;
+  int playerRapier;
+  int playerBattleAxe;
+  int playerLongSword;
+  int playerWeapons;
+  int playerTreasures;
+  int playerArmor;
+  int playerIngredients;
+  int keys;
+  
+  public:
+  
+  void getName();
+  void getFullness();
+  void getGold();
+  void getCeramicPot();
+  void getFryingPan();
+  void getCauldron();
+  void getClub();
+  void getSpear();
+  void getRapier();
+  void getBattleAxe();
+  void getLongSword();
+  void getWeapons();
+  void getTreasures();
+  void getArmor();
+  void getIngredients();
+  void getkeys();
+  
+  string getName(string);
+  int setFullness(int);
+  int setGold(int);
+  int setCeramicPot(int);
+  int setFryingPan(int);
+  int setCauldron(int);
+  int setClub(int);
+  int setSpear(int);
+  int setRapier(int);
+  int setBattleAxe(int);
+  int setLongSword(int);
+  int gsetWeapons(int);
+  int setTreasures(int);
+  int setArmor(int);
+  int setIngredients(int);
+  int setkeys(int);
+  
+  
+  
+};
